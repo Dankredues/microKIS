1886897315a6eb2460fb1c15e844f05f9e065e5e
Daniel Krebs <daniel.krebs@rub.de>
2021-02-09 15:17:50 +0100

    First Working Integration with Vista 120 GW, hardcoded Patients

--- app.py
+++ app.py
@@ -1,44 +1,68 @@
 
 
 from flask import Flask, render_template, session, redirect, url_for, escape, request
-
-import sqlite3
-from datetime import datetime
-import re,os
-import websockets
-import json,time
-from rust_rcon import Command, websocket_connection
-
+from draegertools.patientclass import PatientRecord,HL7Utils
+import hl7
+from hl7.mllp import open_hl7_connection
+import asyncio
 app = Flask(__name__)
 
 
+patientB = PatientRecord(givenName ="Thomas", lastName="Hasse", patientID= "1", bed="Bett1", station="ITS")
+patientA = PatientRecord(givenName ="Patrick", lastName="Giannogonas", patientID= "2", bed="Bett42", station="ITS")
 
-import webapp.draeger_routes
-import webapp.mainroutes
 
 
-@app.route("/admin")
-def admin():
-    if 'username' in session:
-      username = session['username']
-      return render_template("/admin/base.html",username=username)
-    return admin_login()
+beds={ "bed1":patientA,"bed2":patientB,"bed3":None}
 
-@app.route('/admin/login', methods = ['GET', 'POST'])
-def admin_login():
-    if request.method == 'POST':
-      session['username'] = request.form['username']
-      return redirect(url_for('admin'))
+async def sendMesasge(message):
+    hl7_reader, hl7_writer = await asyncio.wait_for(
+        open_hl7_connection("192.168.1.121", 9100),
+        timeout=10,
+    )
 
-    return render_template("/admin/login.html")
+    hl7_message = hl7.parse(message)
+
+    # Write the HL7 message, and then wait for the writer
+    # to drain to actually send the message
+    hl7_writer.writemessage(hl7_message)
+    await hl7_writer.drain()
+    print(f'Sent message\n {hl7_message}'.replace('\r', '\n'))
+
+    # Now wait for the ACK message from the receiever
+    hl7_ack = await asyncio.wait_for(
+      hl7_reader.readmessage(),
+      timeout=10
+    )
+    print(f'Received ACK\n {hl7_ack}'.replace('\r', '\n'))
+
+async def sendHL7Patient(patient):
+    hl7msg = HL7Utils.getHL7ADT(patient)
+    await sendMesasge(hl7msg)
+
+
+async def main():
+    global patientA,patientB
+    print(patientA.givenName)
+    print(patientB.givenName)
+    await sendHL7Patient(patientA)
+    await sendHL7Patient(patientB)
+
+
 
 app.route('/static/<path:path>')
 def send_js(path):
     return send_from_directory('static', path)
 
-@app.route('/admin/logout')
-def logout():
-   # remove the username from the session if it is there
-   session.pop('username', None)
-   return redirect(url_for('admin'))
 
+@app.route("/")
+def index():
+    return render_template("/base.html", beds=beds)
+    
+    
+@app.route("/sendToGW")
+def sendToGW_view():
+    asyncio.set_event_loop(asyncio.SelectorEventLoop())
+    asyncio.get_event_loop().run_until_complete(main())
+    return render_template("/base.html" , infoType=1, message="update Sent!")
+    
